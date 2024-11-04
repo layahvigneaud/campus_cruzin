5be046e7c0baf5c0626189b593ea5acf2fe958d3
angoway <ngoamandauyen@gmail.com>
2024-11-04 15:42:52 -0800

    removed original homepage so that homepage is just where all the club cards will be

--- mern/client/src/components/Home.jsx
+++ mern/client/src/components/Home.jsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import { Link } from "react-router-dom";
 import Navbar from './Navbar';
+import { Link } from "react-router-dom";
 
 function Home() {
     return (
-        <div class="home">
+        <div class="clubs">
             <Navbar/>
-            <div class="home-container">
-                <div class="home-elements">
-                    <h1 className="home-logo">CAMPUS CRUISIN'</h1>
-                    <input class="search-bar" type="text" placeholder="Club Name"></input>
-                    <Link to="/searchclubs">
-                        <button type="button">Search All Clubs</button>
-                    </Link>
-                </div>   
-            </div>
+                <div class="club-cards">
+                    <div class="card-body">
+                        <Link to='/club'>
+                            <h2 class="card-title">Club Name</h2>
+                        </Link>
+                        <p class="card-text">This is a short description of the card content.</p>
+                    </div>
+                </div>
         </div>
     );
 }
