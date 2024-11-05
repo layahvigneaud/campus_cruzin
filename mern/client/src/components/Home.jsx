4d1ab287ab243140ddac4466509b4dd26ea441bd
rchlyoung0415 <rchlmyoung0415@gmail.com>
2024-11-05 10:26:15 -0800

    Added club specific buttons + a list for tags
    The "Learn More" button is just for backend to move on.
    I will be adding a css file later so the whole "card" is
    clickable. The filter list will also be clickable after
    I follow a youtube tutorial.

--- mern/client/src/components/Home.jsx
+++ mern/client/src/components/Home.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Navbar from './Navbar';
 
 function Home() {
+    const filterTags = ["Computer Science", "Social Good", "Aerospace Enginerring"];
     return (
         <div class="home">
             <Navbar/>
@@ -13,6 +14,38 @@ function Home() {
                     <Link to="/searchclubs">
                         <button type="button">Search All Clubs</button>
                     </Link>
+                
+                
+                <div className="filter">
+                    <h2>FILTER TAGS</h2>
+                    <ul>
+                        {filterTags.map((tag, index) => (
+                            <li key={index}>{tag}</li>
+                        ))}
+                    </ul>
+                </div>
+                    
+                <div className="club-card1">
+                    <h2>ACM</h2>
+                    <Link to="/searchclubs">
+                        <button type="button">Learn More</button>
+                    </Link>
+                </div>
+
+                <div className="club-card2">
+                    <h2>LA Blueprint</h2>
+                    <Link to="/searchclubs">
+                        <button type="button">Learn More</button>
+                    </Link>
+                </div>
+
+                <div className="club-card3">
+                    <h2>Rocket Project</h2>
+                    <Link to="/searchclubs">
+                        <button type="button">Learn More</button>
+                    </Link>
+                </div>
+
                 </div>   
             </div>
         </div>
