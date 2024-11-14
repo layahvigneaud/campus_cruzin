0b49b39509faf569b8c81acb0ec200490e3cf2e5
rchlyoung0415 <rchlmyoung0415@gmail.com>
2024-11-13 18:56:27 -0800

    styled Home and Navbar
    basic styling inside of Home.css and Navbar.css to match the figma!

--- mern/client/src/components/Home.jsx
+++ mern/client/src/components/Home.jsx
@@ -1,22 +1,22 @@
 import Navbar from './Navbar';
+import ClubCard from './ClubCard';
 import { Link } from "react-router-dom";
-import '../styles/Home.css';
-import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import '../styles/Home.css';
 import axios from 'axios';
 
 function Home() {
-    const filterTags = ["Computer Science", "Social Good", "Aerospace Engineering"];
     const navigate = useNavigate();
     axios.defaults.withCredentials = true;
 
     useEffect(() => {
         axios.get('http://localhost:3001/auth/verify')
-        .then(response => {
-            if (!response.data.status) {
-                navigate('/');
-            }
-        })
+            .then(response => {
+                if (!response.data.status) {
+                    navigate('/');
+                }
+            })
     }, []);
 
     return (
@@ -24,50 +24,55 @@ function Home() {
             <Navbar />
             <div class="home-container">
                 <div class="home-elements">
-                    <h1 class="home-logo">CAMPUS CRUISIN'</h1>
-                    <input class="search-bar" type="text" placeholder="Club Name"></input>
-                    <Link to="/searchclubs">
-                        <button type="button">Search All Clubs</button>
-                    </Link>
+                    <div class="search-and-cards">
+                        <div class="search-division">
+                            <input class="search-bar" type="text" placeholder="Club Name"></input>
+                            <Link to="/searchclubs">
+                                <button type="button">Search All Clubs</button>
+                            </Link>
+                        </div>
 
-                    <div class="filter">
-                        <h2>FILTER TAGS</h2>
-                        <ul>
-                            {filterTags.map((tag, index) => (
-                                <li key={index}>{tag}</li>
-                            ))}
-                        </ul>
-                    </div>
-                </div>
-            </div>
 
-            <div class="club-cards-container">
-                
-                <div class="side-by-side">
-                    <div class="club-card">
-                        <Link to="/searchclubs">
-                            <button type="button">ACM Teach LA</button>
-                        </Link>
-                    </div>
+                        <div class="club-cards-container">
+                            <div class="side-by-side">
+                                <ClubCard
+                                    title="ACM Teach LA"
+                                    description="ACM Teach LA pairs UCLA students with schools in 
+                                Los Angeles to provide free computer science 
+                                classes. Their goal is to empower students with 
+                                the ability to code, and use it to make a difference."
+                                />
 
-                    <div class="club-card">
-                        <Link to="/searchclubs">
-                            <button type="button">IEEE</button>
-                        </Link>
-                    </div>
-                </div>
+                                <ClubCard
+                                    title="IEEE"
+                                    description="IEEE at UCLA is an engineering organization that 
+                            devotes time and energy towards bringing hands-
+                            on, practical experiences to engineering students 
+                            at UCLA. They lead several projects for members to 
+                            participate in, guided by dedicated leads."
+                                />
+                            </div>
 
-                <div class="side-by-side">
-                    <div class="club-card">
-                        <Link to="/searchclubs">
-                            <button type="button">LA Blueprint</button>
-                        </Link>
-                    </div>
+                            <div class="side-by-side">
+                                <ClubCard
+                                    title="LA Blueprint"
+                                    description=" LA Blueprint is a student-run 501(c)(3) nonprofit
+                            committed to building and promoting tech for Social
+                            good--free of charge. They collaborate with 
+                            nonprofit organizations in Southern California and 
+                            beyond to build mobile and web applications."
+                                />
 
-                    <div class="club-card">
-                        <Link to="/searchclubs">
-                            <button type="button">SWE</button>
-                        </Link>
+                                <ClubCard
+                                    title="SWE"
+                                    description="SWE-UCLA aims to bring professional development
+                            opprotunities to UCLA while advocating for the
+                            creation of an equal platform for all in engineering.
+                            SWE-UCLA has 7 committees and a Grad SWE 
+                            committee for UCLA grad students."
+                                />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
