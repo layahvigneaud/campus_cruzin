4279e4ca3c4e60110a737e433ec916b21432eae5
chrissanrow <chris.sanrow@gmail.com>
2024-11-14 15:26:21 -0800

    Revert "Merge pull request #24 from layahvigneaud/Clubs-page"
    This reverts commit 9fa1ba0ee43c97cac0d973ed25c1136828a1004a, reversing
    changes made to 0ee868f16c9f885266ee92a2b3de868426875430.

--- mern/client/src/components/Club.jsx
+++ mern/client/src/components/Club.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import SaveButton from './SaveButton';
 import Rating from './Rating';
 import ReviewCard from './ReviewCard';
+import ClubInfoCard from './ClubInfoCard';
 import '../styles/Club.css';
 
 function Club() {
@@ -16,37 +17,8 @@ function Club() {
         <div>
             <Navbar/>
             <div className="content-container">
-                <div className="club-info-container">
-                    <SaveButton className="save-button"/>
-                    <div>
-                        <h1>ACM Teach LA</h1>
-                        <p>ACM Teach LA pairs UCLA students with schools in  Los Angeles to provide free computer science classes. Their goal is to  empower all students with the ability to code, and use it to make a  difference.</p>
-                        <Rating className="rating"/>
-                    </div>
-                    <div>
-                        <p>
-                            ACM Teach LA features two main teams: 
-                            <br/>
-                            1. The development team, whose members create learning labs for the schools that Teach LA services.
-                            
-                            2. The curriculum team, whose members teach computer science to local Los Angeles schools on a weekly basis. 
-                        </p>
-                        <p id="descriptors">
-                            <strong>Targeted majors:</strong> Computer Science
-                            <br/>
-                            
-                            <strong>Category:</strong> Social good
-                            <br/>
-                            
-                            <strong>Time commitment:</strong> &lt;5 hours/week
-                            <br/>
-                            
-                            <strong>Application Required?:</strong> No
-                            <br/>
-                            
-                            <strong>Currently Open to Join?:</strong> Yes
-                        </p>
-                    </div>
+                <div>
+                    <ClubInfoCard/>
                 </div>
                 <div className="review-container">
                     <h3>Reviews</h3>
