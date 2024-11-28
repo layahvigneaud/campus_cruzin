d44aaef1b4b46065689d8570aa0f4dddd28a9a73
angoway <ngoamandauyen@gmail.com>
2024-11-28 00:51:16 -0800

    Added save clubs backend and frontend button reflects save state of club

--- mern/client/src/components/ClubCard.jsx
+++ mern/client/src/components/ClubCard.jsx
@@ -7,21 +7,36 @@ import '../styles/ClubCard.css';
 
 function ClubCard({title, description, club_id}) {
     const [reviews, setReviews] = useState([]);
+    const [isSaved, setIsSaved] = useState(false);
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         const fetchReviews = async () => {
             try {
-                console.log('Fetching reviews for club with ID:', club_id);
                 const response = await axios.get(`http://localhost:3001/reviews/${club_id}`);
                 setReviews(response.data);
-                console.log('Reviews:', response.data);
             } catch (error) {
                 console.error('Error fetching reviews:', error);
             }
         };
-
         fetchReviews();
     }, [club_id]);
 
+    useEffect(() => {
+        const getIsSavedClub = async () => {
+            try {
+                const res = await axios.get('http://localhost:3001/auth/user');
+                const savedClubs = res.data.user.savedClubs;
+                const isClubSaved = savedClubs.includes(club_id);
+                setIsSaved(isClubSaved);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching reviews:', error);
+            }
+        };
+        getIsSavedClub();
+    }, [club_id]);
+
+    if (loading) return <p>Loading...</p>;
 
     let overallRating = 0;
     let length = reviews.length;
@@ -39,7 +54,7 @@ function ClubCard({title, description, club_id}) {
                         <Link to={`/club/${club_id}`}>
                             <h3>{title}</h3>
                         </Link>
-                        <SaveClubButton/>
+                        <SaveClubButton saveState={isSaved} clubId={club_id}/>
                     </div>
                 <p>{description}</p>
             </div>
--- mern/client/src/components/SaveClubButton.jsx
+++ mern/client/src/components/SaveClubButton.jsx
@@ -1,13 +1,44 @@
 import React, { useState } from 'react';
 import UnsavedIcon from '../assets/emptybookmark.svg';
 import SavedIcon from '../assets/filledinbookmark.svg';
+import axios from 'axios';
 import '../styles/SaveClubButton.css';
 
-function SaveClubButton({ saveState }) {
-    const [saved, setSaved] = useState(saveState); {/* set to true based on get request from database */}
+function SaveClubButton({ saveState, clubId }) {
+    const [saved, setSaved] = useState(saveState); /* set to true based on get request from database */
     const handleClick = () => {
-        setSaved(!saved);
-        {/* save associated club and update  */}
+        const newSaveState = !saved;
+        try {
+            if (newSaveState)
+            {
+                axios.post('http://localhost:3001/auth/saveClub', { 
+                    clubId 
+                }).then(response => {
+                    if (response.data.status)
+                        console.log("Club successfully saved!");
+                    else
+                        console.log("Something went wrong!");
+                }).catch(err => {
+                    console.log(err);
+                });
+            }
+            else 
+            {
+                axios.post('http://localhost:3001/auth/unsaveClub', {
+                    clubId
+                }).then(response => {
+                    if (response.data.status)
+                        console.log("Club successfully saved!");
+                    else 
+                        console.log("Something went wrong!");
+                }).catch(err => {
+                    console.log(err);
+                });
+            }
+            setSaved(newSaveState);        
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     return (
