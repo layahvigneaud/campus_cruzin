08405bee60c69dd21bc41a0b5d48c696dc8d49bf
angoway <ngoamandauyen@gmail.com>
2024-11-27 22:15:49 -0800

    Toggle shading of bookmark button

--- mern/client/src/components/SaveClubButton.jsx
+++ mern/client/src/components/SaveClubButton.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
 import UnsavedIcon from '../assets/emptybookmark.svg';
+import SavedIcon from '../assets/filledinbookmark.svg';
 import '../styles/SaveClubButton.css';
 
-function SaveClubButton() {
-    const [saved, setSaved] = useState(false); {/* set to true based on get request from database */}
-
+function SaveClubButton({ saveState }) {
+    const [saved, setSaved] = useState(saveState); {/* set to true based on get request from database */}
     const handleClick = () => {
+        setSaved(!saved);
         {/* save associated club and update  */}
     }
 
     return (
         <button onClick={ handleClick } className="save-club-button">
             {/* save icon based on save status */}
-            <img src={saved ? UnsavedIcon : UnsavedIcon}/>
+            <img src={saved ? SavedIcon : UnsavedIcon}/>
         </button>
     );
 }
