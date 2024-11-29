4d1479912f3d2328f07d5c48c3b186ba646cc391
rchlyoung0415 <rchlmyoung0415@gmail.com>
2024-11-28 21:39:22 -0800

    initial navbar set up
    styling will come later! this just adds the save button
    and home button to the navbar.

--- mern/client/src/components/Navbar.jsx
+++ mern/client/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 import LogoutButton from './LogoutButton'
+import HomeIcon from '../assets/home.svg';
 import '../styles/Navbar.css';
 
 function Navbar() {
@@ -8,6 +9,14 @@ function Navbar() {
             <div class="navbar-side-by-side">
                 <Link to="/review">
                     <button type="button">Submit a Review!</button>
+                </Link>
+                <Link to="/savedclubs">
+                    <button type="button">Your Saved Clubs</button>
+                </Link>
+                <Link to="/homepage">
+                    <button type="button">
+                        <img src={HomeIcon}/>
+                    </button>
                 </Link>
                 <div class="navbar-logout-button"> {
                     <LogoutButton/>
