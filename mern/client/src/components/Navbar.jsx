0478a1113274bd2421dae8c18ba9282d77223aa9
rchlyoung0415 <rchlmyoung0415@gmail.com>
2024-11-29 20:10:57 -0800

    added saved reviews to navbar
    changed icon for saved reviews to be a heart instead
    and linked savedreviews to the navbar

--- mern/client/src/components/Navbar.jsx
+++ mern/client/src/components/Navbar.jsx
@@ -2,7 +2,8 @@ import { Link } from "react-router-dom"
 import LogoutButton from './LogoutButton'
 import HomeIcon from '../assets/home.svg';
 import SavedIcon from '../assets/filledinbookmark.svg';
-import ReviewIcon from '../assets/reviewbubble.svg';
+import ReviewIcon from '../assets/review.svg';
+import SavedReviewIcon from '../assets/filledheart.svg';
 import '../styles/Navbar.css';
 
 function Navbar() {
@@ -18,6 +19,11 @@ function Navbar() {
                     <Link to="/savedclubs">
                         <button type="button">
                             <img src={SavedIcon} />
+                        </button>
+                    </Link>
+                    <Link to="/savedreviews">
+                        <button type="button">
+                            <img src={SavedReviewIcon} />
                         </button>
                     </Link>
                     <Link to="/homepage">
