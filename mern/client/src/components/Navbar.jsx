9124b9bcd384752e635d50633cba39dd618cf7b1
rchlyoung0415 <rchlmyoung0415@gmail.com>
2024-11-29 19:12:32 -0800

    navbar styling
    changed the words into icons for review, saved clubs and home
    need to make the bookmark icon able to go to saved clubs/icons

--- mern/client/src/components/Navbar.jsx
+++ mern/client/src/components/Navbar.jsx
@@ -1,26 +1,35 @@
 import { Link } from "react-router-dom"
 import LogoutButton from './LogoutButton'
 import HomeIcon from '../assets/home.svg';
+import SavedIcon from '../assets/filledinbookmark.svg';
+import ReviewIcon from '../assets/reviewbubble.svg';
 import '../styles/Navbar.css';
 
 function Navbar() {
     return (
         <div class="navbar">
             <div class="navbar-side-by-side">
-                <Link to="/review">
-                    <button type="button">Submit a Review!</button>
-                </Link>
-                <Link to="/savedclubs">
-                    <button type="button">Your Saved Clubs</button>
-                </Link>
-                <Link to="/homepage">
-                    <button type="button">
-                        <img src={HomeIcon}/>
-                    </button>
-                </Link>
-                <div class="navbar-logout-button"> {
-                    <LogoutButton/>
-                }
+                <div class="navbar-container">
+                    <Link to="/review">
+                        <button type="button">
+                            <img src={ReviewIcon} />
+                        </button>
+                    </Link>
+                    <Link to="/savedclubs">
+                        <button type="button">
+                            <img src={SavedIcon} />
+                        </button>
+                    </Link>
+                    <Link to="/homepage">
+                        <button type="button">
+                            <img src={HomeIcon} />
+                        </button>
+                    </Link>
+                </div>
+                <div class="navbar-logout-button">
+                    {
+                        <LogoutButton />
+                    }
                 </div>
             </div>
         </div>
